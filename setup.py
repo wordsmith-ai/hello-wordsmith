4a211f6645765a50e6b49c91ae4ea26bf77b3cde
Ross McNairn <ross.mcnairn.dev@wordsmith.ai>
2024-05-02 01:38:38 +0100

    strip down deps and version lock

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="hello-wordsmith",
     version="0.1.0",
-    description="A simple Python package to interface with llama-index RAG over wordsmith data.",
+    description="A simple Python package to wrap llama-index RAG CLI over Wordsmith data.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://huggingface.co/datasets/derek-at-work/test/",
@@ -14,13 +14,19 @@ setup(
     package_data={
         'hello_wordsmith': ['public_wordsmith_dataset/*'],
     },
-    install_requires=["llama-index", "chromadb", "llama-index-vector-stores-chroma"],
+    install_requires=[
+        "chromadb~=0.5.0",
+        "llama-index-core~=0.10.33",
+        "llama-index-llms-openai~=0.1.16",
+        "llama-index-embeddings-openai~=0.1.9",
+        "llama-index-vector-stores-chroma~=0.1.7",
+    ],
     classifiers=[
         "Intended Audience :: End Users",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     entry_points={
         "console_scripts": [
